3a06592cac9b45a31f37ae0ccdaa15695c91f001
Jad Abdallah <justcallmethis@gmail.com>
2018-12-21 14:29:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,13 +4,12 @@ class App < Sinatra::Base
 
 
   get '/newteam' do 
-          binding.pry
     erb :newteam 
     
   end 
   
   post '/team' do 
-          binding.pry
+          
     @team = params 
     # erb :team
   end 
